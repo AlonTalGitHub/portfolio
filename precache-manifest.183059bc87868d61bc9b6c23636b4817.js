2b4c224d87dc88dc57f9043ee090569ec08c1413
Alon Tal <alontal23@gmail.com>
2020-08-26 15:07:53 +0300

    Updates

---
+++ precache-manifest.183059bc87868d61bc9b6c23636b4817.js
@@ -0,0 +1,54 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "53d79d490d68f70995bc6f359392736a",
+    "url": "/portfolio/index.html"
+  },
+  {
+    "revision": "8a62bc8dd3284c15a3a2",
+    "url": "/portfolio/static/css/2.4707e12a.chunk.css"
+  },
+  {
+    "revision": "8a62bc8dd3284c15a3a2",
+    "url": "/portfolio/static/js/2.ee0b353d.chunk.js"
+  },
+  {
+    "revision": "0749163b59fbee32225059cb60c18af6",
+    "url": "/portfolio/static/js/2.ee0b353d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "94e4c09097ed319a39b3",
+    "url": "/portfolio/static/js/main.e5864664.chunk.js"
+  },
+  {
+    "revision": "4113d93a276f2b41f70f",
+    "url": "/portfolio/static/js/runtime-main.83eeea52.js"
+  },
+  {
+    "revision": "5ab8d9e3b4e391f94d6c3c95ed7818ca",
+    "url": "/portfolio/static/media/NeueHaasDisplay-Mediu.5ab8d9e3.woff"
+  },
+  {
+    "revision": "127d6b3dd0ba04112bd87916c59640f3",
+    "url": "/portfolio/static/media/NeueHaasDisplay-Roman.127d6b3d.woff"
+  },
+  {
+    "revision": "124964a278085a5bc8fa3e67e256402f",
+    "url": "/portfolio/static/media/alon.657194b2.png"
+  },
+  {
+    "revision": "12312b07e981fc9bb5c81aa6a7845b29",
+    "url": "/portfolio/static/media/buddhas.9638b42c.jpg"
+  },
+  {
+    "revision": "92dd8ba646f0921c64c4e9aad27e9aca",
+    "url": "/portfolio/static/media/cv.92dd8ba6.pdf"
+  },
+  {
+    "revision": "8c6d99ef95dc417c4cc9c4d53309e499",
+    "url": "/portfolio/static/media/logo.61779323.png"
+  },
+  {
+    "revision": "9dbd26f49c2cbdd9df1ebb7663cbb8e0",
+    "url": "/portfolio/static/media/routes.bc62456c.jpg"
+  }
+]);
